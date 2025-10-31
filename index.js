7a6b07fb35107a259d8b1eff9b32340f31a6ea3a
Kaan Özkilic <kaanic1337@gmail.com>
2025-10-31 22:16:40 +0200

    created basic slash commands

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(process.env.DISCORD_TOKEN);
+// require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// creating a new client instance
+// guild = discord server
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// when the client is ready, run this code (only once).
+// it makes some properties non-nullable.
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// log in to discord with your token
+client.login(token);
